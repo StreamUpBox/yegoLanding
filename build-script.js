2e9810087fbc30433fece97d3120b309367a03be
=richie <=beastar457@gmail.com>
2018-07-23 14:34:08 +0200

    w

---
+++ build-script.js
@@ -0,0 +1,18 @@
+const fs = require('fs-extra');
+const concat = require('concat');    
+
+(async function build() {
+
+    const files =[
+        './dist/pw/runtime.js',
+        './dist/pw/polyfills.js',
+        './dist/pw/main.js'
+    ]
+    
+    await fs.ensureDir('elements')
+    
+    await concat(files, 'elements/rwpay.js')
+    console.info('Elements created successfully!')
+
+})()
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { PaymentComponent } from './payment/payment.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    PaymentComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents:[PaymentComponent],
+  bootstrap: []
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const el = createCustomElement(PaymentComponent, { injector: this.injector });
+    
+    customElements.define('rw-pay', el);
+   }
+}
